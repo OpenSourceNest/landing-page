ec0e038f6ee5b8d880945a6b0449db2277a78ff5
Francis Onukwu <onukwuf@gmail.com>
2025-07-16 19:02:46 +0100

    chore: prepare repo

---
+++ src/font/ClashDisplay.font.ts
@@ -0,0 +1,11 @@
+import localFont from "next/font/local";
+
+const ClashDisplay = localFont({
+  src: [
+    {
+      path: "../../public/fonts/ClashDisplay-Variable.woff2",
+    },
+  ],
+});
+
+export default ClashDisplay;
