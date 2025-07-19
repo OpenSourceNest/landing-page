86e45c34f70f60970a1f29dbc6a8da07bc47ecbf
Francis Onukwu <onukwuf@gmail.com>
2025-07-20 00:05:23 +0100

    chore: add og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,30 @@
+import ClashDisplayFont from "@/font/ClashDisplay.font";
 import type { Metadata } from "next";
 import "./globals.css";
-import ClashDisplayFont from "@/font/ClashDisplay.font";
+import OG from "./opengraph-image.png";
 
 export const metadata: Metadata = {
   title: "Open Source Nest",
   description: "",
+  metadataBase: new URL("https://opensourcenest.org"),
+  openGraph: {
+    images: [
+      {
+        url: OG.src,
+        width: OG.width,
+        height: OG.height,
+      },
+    ],
+  },
+  twitter: {
+    images: [
+      {
+        url: OG.src,
+        width: OG.width,
+        height: OG.height,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
