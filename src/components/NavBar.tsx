04b6a4493d7ee5744ac532700a76856614d9e9ed
Francis Onukwu <onukwuf@gmail.com>
2025-07-31 12:22:00 +0100

    feat: add sidebar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,35 +1,93 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import AnimatedHamburgerButton from "./ui/AnimatedHamburgerButton";
+import { useState } from "react";
 
 const NavBar = () => {
+  const [displaySidebar, setDisplaySidebar] = useState(true);
+
+  const hideSideBar = () => {
+    setDisplaySidebar(false);
+  };
+
   return (
-    <nav
-      className="flex w-[1146px] items-center border rounded-full border-[#2A2A2A]
-    justify-between gap-3 py-4 px-16 max-w-[95vw] mx-auto mt-[40px]"
+    <div
+      className={
+        displaySidebar
+          ? `no-doc-scroll flex flex-col h-screen fixed w-full z-[12] mt-[20px] px-2 gap-4
+          pb-2`
+          : "mt-[40px] sticky top-2.5"
+      }
     >
-      <Image
-        src="/images/logo-full.png"
-        alt="OSN Logo"
-        height={52}
-        width={212}
-      />
+      <nav
+        className="flex w-[1146px] items-center border rounded-full border-[#2A2A2A]
+      justify-between gap-3 py-4 px-16 max-w-[95vw] mx-auto
+      z-20 backdrop-blur-lg bg-[#0000007b]"
+      >
+        <Image
+          src="/images/logo-full.png"
+          alt="OSN Logo"
+          height={52}
+          width={212}
+        />
 
-      <div className="flex items-center gap-3">
-        {NavLinks.map((nav) => (
-          <Link href={nav.url} key={nav.title} className="nav-link">
-            <p className="font-medium mb-0.5">{nav.title}</p>
-            <span className="block w-0 h-0.5 bg-primary mx-auto"></span>
-          </Link>
-        ))}
+        <div className="flex items-center gap-3 max-[1100px]:hidden">
+          {NavLinks.map((nav) => (
+            <Link href={nav.url} key={nav.title} className="nav-link">
+              <p className="font-medium mb-0.5">{nav.title}</p>
+              <span className="block w-0 h-0.5 bg-primary mx-auto"></span>
+            </Link>
+          ))}
 
-        <button
-          className="py-2.5 px-6 font-semibold text-primary border border-primary 
-        rounded-full ml-3"
-        >
-          JOIN COMMUNITY
-        </button>
+          <button
+            className="py-2.5 px-6 font-semibold text-primary border border-primary 
+          rounded-full ml-3"
+          >
+            JOIN COMMUNITY
+          </button>
+        </div>
+
+        <AnimatedHamburgerButton
+          setActive={setDisplaySidebar}
+          active={displaySidebar}
+        />
+      </nav>
+
+      {/* -------------------------------------------------------------------------- */
+      /*                                   SIDEBAR                                  */
+      /* -------------------------------------------------------------------------- */}
+      <div
+        className={`block border border-[#2A2A2A] h-[calc(100%_-_110px)] rounded-3xl
+      bg-[#0000007b] backdrop-blur-2xl max-w-[95vw] mx-auto w-full p-5 justify-center
+      overflow-y-auto min-[1100px]:hidden ${
+        displaySidebar ? "flex" : "hidden"
+      }`}
+      >
+        <div className="gap-5 flex flex-col items-center mb-[350px]">
+          {NavLinks.map((nav) => (
+            <Link
+              href={nav.url}
+              key={nav.title}
+              className="nav-link"
+              onClick={hideSideBar}
+            >
+              <p className="font-medium mb-0.5 text-[20px]">{nav.title}</p>
+              <span className="block w-0 h-0.5 bg-primary mx-auto"></span>
+            </Link>
+          ))}
+
+          <button
+            className="py-2.5 px-6 font-semibold text-primary border border-primary 
+          rounded-full ml-3"
+            onClick={hideSideBar}
+          >
+            JOIN COMMUNITY
+          </button>
+        </div>
       </div>
-    </nav>
+    </div>
   );
 };
 
---
+++ src/components/ui/AnimatedHamburgerButton.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { MotionConfig, motion } from "framer-motion";
+
+interface IProp {
+  active: boolean;
+  setActive: (data: boolean) => void;
+}
+
+const AnimatedHamburgerButton = ({ active, setActive }: IProp) => {
+  return (
+    <MotionConfig
+      transition={{
+        duration: 0.3,
+        ease: "linear",
+      }}
+    >
+      <motion.button
+        initial={false}
+        animate={active ? "open" : "closed"}
+        onClick={() => setActive(!active)}
+        className="relative h-8 w-8 rounded bg-white/0 transition-colors hover:bg-white/20 scale-125
+        min-[1100px]:hidden"
+      >
+        <motion.span
+          variants={VARIANTS.top}
+          className="absolute h-0.5 w-5 bg-white"
+          style={{ y: "-50%", left: "50%", x: "-50%", top: "35%" }}
+        />
+        <motion.span
+          variants={VARIANTS.middle}
+          className="absolute h-0.5 w-5 bg-white"
+          style={{ left: "50%", x: "-50%", top: "50%", y: "-50%" }}
+        />
+        <motion.span
+          variants={VARIANTS.bottom}
+          className="absolute h-0.5 w-2.5 bg-white"
+          style={{
+            x: "-50%",
+            y: "50%",
+            bottom: "35%",
+            left: "calc(50% + 10px)",
+          }}
+        />
+      </motion.button>
+    </MotionConfig>
+  );
+};
+
+const VARIANTS = {
+  top: {
+    open: {
+      rotate: ["0deg", "0deg", "45deg"],
+      top: ["35%", "50%", "50%"],
+    },
+    closed: {
+      rotate: ["45deg", "0deg", "0deg"],
+      top: ["50%", "50%", "35%"],
+    },
+  },
+  middle: {
+    open: {
+      rotate: ["0deg", "0deg", "-45deg"],
+    },
+    closed: {
+      rotate: ["-45deg", "0deg", "0deg"],
+    },
+  },
+  bottom: {
+    open: {
+      rotate: ["0deg", "0deg", "45deg"],
+      bottom: ["35%", "50%", "50%"],
+      left: "50%",
+    },
+    closed: {
+      rotate: ["45deg", "0deg", "0deg"],
+      bottom: ["50%", "50%", "35%"],
+      left: "calc(50% + 5px)",
+    },
+  },
+};
+
+export default AnimatedHamburgerButton;
